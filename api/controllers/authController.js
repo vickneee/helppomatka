53812663478273b8c032d82815c1f2b334593680
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-03-19 23:10:43 +0200

    Update authController.js

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
-import { createError } from "../utils/error.js";
+import {createError} from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
 // REGISTER a User
@@ -12,48 +12,42 @@ export const register = async (req, res, next) => {
 
         // CREATING a New User
         const newUser = new User({
-            username:req.body.username,
-            email:req.body.email,
-            password: hash,
-      });
+            username: req.body.username, email: req.body.email, password: hash,
+        });
 
-      await newUser.save();
-      res.status(200).send("User has been created.");
+        const savedUser = await newUser.save();
+        // await newUser.save();
+
+        // Processing TOKEN with JWT SECRET KEY
+        const token = jwt.sign({id: savedUser._id, isAdmin: savedUser.isAdmin}, process.env.JWT_SECRET_KEY);
+
+        res.status(200).json({token, userId: savedUser._id, message: "User has been created."});
     } catch (err) {
-      next(err);
+        next(err);
     }
-  };
+};
 
 // LOGIN
-  export const login = async (req, res, next) => {
+export const login = async (req, res, next) => {
     try {
-      const user = await User.findOne({ username: req.body.username });
-      if (!user) return next(createError(404, "User not found!"));
+        const user = await User.findOne({username: req.body.username});
+        if (!user) return next(createError(404, "User not found!"));
 
 
-      // PASSWORD Checker
-      const isPasswordCorrect = await bcrypt.compare(
-        req.body.password,
-        user.password
-      );
-      if (!isPasswordCorrect)
-        return next(createError(400, "password or username not correct!"));
+        // PASSWORD Checker
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user['password']);
+        if (!isPasswordCorrect) return next(createError(400, "password or username not correct!"));
 
-      // Processing TOKEN with JWT SECRET KEY
-      const token = jwt.sign(
-        { id: user._id, isAdmin: user.isAdmin },
-        process.env.JWT_SECRET_KEY
-      );
+        // Processing TOKEN with JWT SECRET KEY
+        const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET_KEY);
 
-      // Processing ACCESS Token Status
-      const { password, isAdmin, ...otherDetails } = user._doc;
-      res
-        .cookie("access_token", token, {
-          httpOnly: true,
-        })
-        .status(200)
-        .json({ details: { ...otherDetails }, isAdmin });
+        // Processing ACCESS Token Status
+        const {password, isAdmin, ...otherDetails} = user._doc;
+        res
+            .status(200)
+            .json({details: {...otherDetails}, isAdmin, token}); // Added Token to the Response
     } catch (err) {
-      next(err);
+        next(err);
     }
-  };
+};
+
