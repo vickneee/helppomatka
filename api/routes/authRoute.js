c0c60ef39f97dba76fc39d3038b0adee1837c58f
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-03-19 22:55:31 +0200

    Update autRoute.js

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -3,6 +3,10 @@ import { login, register } from "../controllers/authController.js";
 
 const router = express.Router();
 
+// Route Middlewares
+// This will prefix all routes with '/auth' (e.g. '/api/auth/register', '/api/auth/login')
+// app.use('/api/auth', AuthRoute);
+
 // REGISTER Path
 router.post("/register", register)
 
